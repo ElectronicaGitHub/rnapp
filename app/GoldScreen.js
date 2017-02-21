6b804134a7d8bf156f3cf64ba29a36eb50c26925
Филипп <filipp@Mac-mini-Filipp.local>
2017-02-21 19:36:28 +0300

    f

---
+++ app/GoldScreen.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+class GoldScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={styles.welcome}
+          onPress={() => Actions.black()}
+        >
+          Gold Screen
+        </Text>
+        <Text
+          style={styles.welcome}
+          onPress={() => Actions.modal()}
+        >
+          Open Modal
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#997F3D',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+});
+
+export default GoldScreen;
